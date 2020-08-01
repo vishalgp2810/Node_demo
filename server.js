707569424e88145819e8ab3af5aa1037f7cc73ed
vishalghodke98 <vishalghodke@kudosfinance.in>
2020-08-01 21:05:39 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = 8000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(` app is running on port =>:${port}`));
